1f8a0e3e151ae714f81069079cbc55ebd6dbdb4b
cmathez <celinemathez@gmail.com>
2021-08-05 14:56:13 +0200

    first commit

---
+++ src/main/java/com/inti/presentation/App.java
@@ -0,0 +1,13 @@
+package com.inti.presentation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
